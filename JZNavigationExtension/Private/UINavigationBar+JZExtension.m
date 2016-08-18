565db63e760180f34a07f13fe04078f3100fca14
wells <wells1013@163.com>
2016-08-18 09:14:39 +0800

    如果是隐藏状态的话，透明的， 那么将事件向下传递，不处理事件

--- JZNavigationExtension/Private/UINavigationBar+JZExtension.m
+++ JZNavigationExtension/Private/UINavigationBar+JZExtension.m
@@ -13,6 +13,10 @@ JZExtensionBarImplementation
 
 - (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent *)event {
     if ([[self jz_backgroundView] alpha] < 1.0f) {
+        //如果是隐藏状态的话，透明的， 那么将事件向下传递，不处理事件
+        if ([[self jz_backgroundView] alpha] - 0.0f <= 0.0001) {
+            return NO;
+        }
         return CGRectContainsPoint(CGRectMake(0, self.bounds.size.height - JZNavigationBarHeight, self.bounds.size.width, JZNavigationBarHeight), point);
     } else {
         return [super pointInside:point withEvent:event];
